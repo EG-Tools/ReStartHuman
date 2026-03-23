fcfeb23056585f795ce82b7e68857772f8eecf97
EG-Tools <keg0320@gmail.com>
2026-03-24 01:39:06 +0800

    Update QuestionScreen.tsx

--- src/components/question/QuestionScreen.tsx
+++ src/components/question/QuestionScreen.tsx
@@ -299,7 +299,7 @@ function QuestionNumberFields({
                     lineHeight: 1,
                     flexShrink: 0,
                     whiteSpace: 'nowrap',
-                    marginTop: '1px',
+                    marginTop: '2px',
                   }}
                 >
                   {suffix}
