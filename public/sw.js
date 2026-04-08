1978a63658ed2be04fc3b9cd9526e45ce3aa38ee
keg0320 <keg0320@gmail.com>
2026-04-08 15:49:28 +0800

    Show input selection highlight

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'restarthuman-alpha-v46'
+const CACHE_NAME = 'restarthuman-alpha-v47'
 const toScopedUrl = (path) => new URL(path, self.registration.scope).toString()
 const APP_SHELL = [
   toScopedUrl('./'),
