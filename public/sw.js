370cc8384708aee8f9210b5832be8a2d0a8aa4a3
keg0320 <keg0320@gmail.com>
2026-08-23 15:22:09 +0900

    Reveal hero city details

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'restarthuman-alpha-v60'
+const CACHE_NAME = 'restarthuman-alpha-v61'
 const toScopedUrl = (path) => new URL(path, self.registration.scope).toString()
 const APP_SHELL = [
   toScopedUrl('./'),
